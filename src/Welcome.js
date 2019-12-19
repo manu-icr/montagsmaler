f42f92d11a8c0c2db67b5133ed622cc8ad93b704
Manuel <manuel.suarez@icr.ch>
2019-12-19 14:52:22 +0100

    text wrapping optimized

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TextBlock from "./TextBlock";
 import text from "./text.json";
 function Welcome() {
-  return (<TextBlock strings={[text.welcome]} />);
+  return (<TextBlock strings={text.welcome} />);
 }
 
 
