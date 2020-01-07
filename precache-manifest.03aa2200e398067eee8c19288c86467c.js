3e0e00c2682bd5ecd222d2cc5b8805cc536ae6f0
Manuel <manuel.suarez@icr.ch>
2020-01-07 11:38:54 +0100

    Updates

---
+++ precache-manifest.03aa2200e398067eee8c19288c86467c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d59aafcd41204451101f50ddf929268",
+    "url": "/project-react-c1-s8-209/index.html"
+  },
+  {
+    "revision": "2c96974c46cc47a5e72e",
+    "url": "/project-react-c1-s8-209/static/css/main.d9240828.chunk.css"
+  },
+  {
+    "revision": "5d57534a90af6cab6850",
+    "url": "/project-react-c1-s8-209/static/js/2.947cc8cd.chunk.js"
+  },
+  {
+    "revision": "2c96974c46cc47a5e72e",
+    "url": "/project-react-c1-s8-209/static/js/main.1f5a0e0f.chunk.js"
+  },
+  {
+    "revision": "33e57f13effa763f16f6",
+    "url": "/project-react-c1-s8-209/static/js/runtime-main.2e254e25.js"
+  }
+]);
